d50858d39c1bb3e4f61e1f9484277c9e8a136c8b
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-21 16:53:06 -0500

    CT completed - declared DOM variables, added submit listener, called created functions

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    console.log("page loaded")
+    
+    // Declaring & Initializing Form element from DOM
+    const form = document.querySelector("form");
+    
+    
+    // Using Event Listener to listen for a 'submit' event
+    form.addEventListener("submit", function(event) {
+        
+        // Declaring & Initializing Form elements from DOM
+        let list = document.getElementById("faultyItems");
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
+ 
+        // CT NOTE: make sure formSubmission (do "fuelLevel.value") when calling the variables
+        formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+        
+        // Prevent Form Submission
+        event.preventDefault();
+    });
+    
+
+
 
     let listedPlanets;
+
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
+
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
+
     }).then(function () {
         console.log(listedPlanets);
+
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
-    
+        let planetSelected = pickPlanet(listedPlanets);
+        console.log(planetSelected);
+
+        // Displays the Mission Destination
+        addDestinationInfo(document, planetSelected.name, planetSelected.diameter, planetSelected.star, planetSelected.distance, planetSelected.moons, planetSelected.image);
+    });
  });
